095e61db04929c1166c31ddb60895acc2ec8db93
EC2 Default User <ec2-user@ip-172-31-43-117.ec2.internal>
2019-05-04 07:11:40 +0000

    changes made

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <?php include 'navbar.php'; ?>
-    <h1>Welcome!<h1>
+    <h1>Welcome!</h1>
     <div class="holder">
         <p>
 		Welcome to Square Dodge Game! Square Dodge Game is simple yet additive. You are a tiny
@@ -19,18 +19,20 @@
         </p>
         <img src="images/logo.png" alt="logo.phg"></img>
     </div>
+    <hr>
     <h2>I Challenge You!</h2>
     <div class="holder">
        <p>Here is a sample video of the game. It is also the highest on the leaderboard! I challenge you to beat it</p>
-       <iframe width="400" height="300" src="https://www.youtube.com/watch?v=5p_YjxRMmp8">
+       <iframe width="400" height="300" src="https://www.youtube.com/embed/5p_YjxRMmp8">
        </iframe>
     </div>  
     </div>
+    <hr>
     <h2>Gallary</h2>
     <div class="holder">
         <?php
             for($i = 1;$i <= 5; $i++)
-                echo '<img src="images/game'.$i.'.php" alt="game'.$i.'"></img>'."\n";
+                echo '<img src="images/game'.$i.'.png" alt="game'.$i.'" width="680" height="400"></img>'."\n";
         ?>
     </div>
 </body>
